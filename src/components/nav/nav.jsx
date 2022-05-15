76d9d6169c38db80a573dff35b8dcd7e12d63830
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-15 22:41:57 +0200

    add nav section

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import { RiServiceLine } from 'react-icons/ri'
+import {FiMessageCircle} from 'react-icons/fi'
 
 function nav() {
   return (
-    <div>nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BiBook /></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><FiMessageCircle/></a>
+    </nav>
   )
 }
 
