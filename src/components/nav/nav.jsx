ec2e800786259021d99e84fe959830b29bb77829
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-15 23:37:27 +0200

    modify the floating nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import './nav.css'
-import { AiOutlineHome } from 'react-icons/ai'
-import { AiOutlineUser } from 'react-icons/ai'
-import { BiBook } from 'react-icons/bi'
-import { RiServiceLine } from 'react-icons/ri'
-import {FiMessageCircle} from 'react-icons/fi'
+import React from "react";
+import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { FiMessageCircle } from "react-icons/fi";
+import { useState } from "react";
 
 function nav() {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#"><AiOutlineHome /></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><BiBook /></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><FiMessageCircle/></a>
+      <a href="#" className={activeNav === '"#' ? 'active' : ''}>
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <BiBook />
+      </a>
+      <a href="#services">
+        <RiServiceLine />
+      </a>
+      <a href="#contact">
+        <FiMessageCircle />
+      </a>
     </nav>
-  )
+  );
 }
 
-export default nav
+export default nav;
